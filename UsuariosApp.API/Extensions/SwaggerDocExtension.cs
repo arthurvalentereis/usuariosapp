8b677c71ba3deda1c6c7f991b315058b6e800949
arthurvalentereis <areis@infisconsultoria.com.br>
2023-05-08 10:43:27 -0300

    initial commit

---
+++ UsuariosApp.API/Extensions/SwaggerDocExtension.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.OpenApi.Models;
+using System.Reflection;
+
+namespace UsuariosApp.API.Extensions
+{
+    public static class SwaggerDocExtension
+    {
+        public static IServiceCollection AddSwaggerDoc(this IServiceCollection services)
+        {
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "UsuárioAPP - API - Treinamento Infis",
+                    Description = " API para controlde de usuários",
+                    Version = "v1", 
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Test",
+                        Email = "contato@cotiinformatica.com",
+                        Url = new Uri("https://www.cotiinformatica.com.br")
+                    }
+                });
+                //Jogar no arquivo do swagger os comentários da api
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                options.IncludeXmlComments(xmlPath);
+            });
+
+            return services;
+        }
+        public static IApplicationBuilder UseSwaggeDoc(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "UsuariosApp.API");
+            });
+
+            return app;
+        }
+    }
+}
---
+++ UsuariosApp.API/Program.cs
@@ -0,0 +1,21 @@
+using UsuariosApp.API.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddRouting( options => options.LowercaseUrls = true );
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddSwaggerDoc();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseSwaggeDoc();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
